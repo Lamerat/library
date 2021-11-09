a8b92f9a3f21399e13dc9b67872a5a35be7e8af7
Simeon Mladenov <mladenov.simo@gmail.com>
2021-11-09 12:54:48 +0200

    again fix

--- src/authentication/create-token.js
+++ src/authentication/create-token.js
@@ -1,8 +1,10 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
-const JWT_KEY = dotenv.config().parsed.JWT_KEY;
-const TOKEN_LIFE = dotenv.config().parsed.TOKEN_LIFE;
+// const JWT_KEY = dotenv.config().parsed.JWT_KEY;
+const JWT_KEY = 'R4DI4TOR';
+// const TOKEN_LIFE = dotenv.config().parsed.TOKEN_LIFE;
+const TOKEN_LIFE = '24h';
 
 const createToken = (payLoad) => jwt.sign(payLoad, JWT_KEY, { expiresIn: TOKEN_LIFE });
 
